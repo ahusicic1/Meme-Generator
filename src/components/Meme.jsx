b94590b5496a3243340e9dc8fe5c67613895e4d6
ahusicic1 <ahusicic1@etf.unsa.ba>
2023-08-29 15:45:20 +0200

    using states

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,9 +1,36 @@
 import React from "react"
+import data from "../memeData.js"
+import { RANDOM } from "mysql/lib/PoolSelector.js"
 
 export default function Meme(props){
+    var memes = data.data.memes
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        image: "http://i.imgflip.com/1bij.jpg" 
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(data)
+
+function writeURL(){
+    var memesArray = allMemeImages.data.memes
+    var randomImageUrl = memesArray[
+                    Math.floor(
+                            Math.random()*memesArray.length
+                            )]
+                            .url
+
+    setMeme(prevMeme => {
+        return {
+            ...prevMeme,
+            image: randomImageUrl
+        }
+    })
+}
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input
                      type="text"
                      placeholder="Top text"
@@ -13,8 +40,10 @@ export default function Meme(props){
                      placeholder="Bottom text"
                      className="form--input"/>
                 <button 
-                    className="form--button">Get a new meme image</button>
-            </form>
+                    className="form--button"
+                    onClick={writeURL} >Get a new meme image</button>
+            </div>
+            <img src = {meme.image} className="meme--image" />
         </main>
     )
 }
