efabddd11c22ccd468442418fded6a01166554d0
ahusicic1 <ahusicic1@etf.unsa.ba>
2023-08-31 13:05:10 +0200

    add text to image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -26,24 +26,49 @@ function writeURL(){
             image: randomImageUrl
         }
     })
-}
+   }
 
+   function handleChange(event){
+    const {name, value} = event.target 
+    console.log(name, value)
+    console.log(meme)
+    setMeme( prevMeme => {
+        return {
+            ...prevMeme,
+            [name]: value
+        }
+    })
+}
+  
     return (
         <main>
             <div className="form">
                 <input
                      type="text"
                      placeholder="Top text"
-                     className="form--input"/>
+                     className="form--input"
+                     name= "topText"
+                     value= {meme.topText}
+                     onChange={handleChange}
+                     />
                 <input
                      type="text"
                      placeholder="Bottom text"
-                     className="form--input"/>
+                     className="form--input"
+                     name= "bottomText"
+                     value= {meme.bottomText}
+                     onChange={handleChange}
+                     />
                 <button 
                     className="form--button"
                     onClick={writeURL} >Get a new meme image</button>
             </div>
+
+            <div className="meme">
             <img src = {meme.image} className="meme--image" />
+            <h2 className="meme--text top">{meme.topText}</h2>
+            <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
